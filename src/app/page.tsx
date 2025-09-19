60f5923defd135de2d8f6759eeec3a21c2f8c449
trickyBytes <richard.tiffin@ovo.com>
2025-09-19 11:09:10 +0100

    Simple authentication implemented against firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-import SignInForm from "@/components/SignInForm";
+"use client";
+
 import GoogleSignInButton from "@/components/GoogleSignInButton";
+import { useAuth } from "../components/AuthContext";
 
 export default function Home() {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <SignInForm />
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      {user ? <p>Welcome, {user.displayName}!</p> : <p>Please sign in.</p>}
+      <div className="mt-4" />
       <GoogleSignInButton />
     </main>
-  )
+  );
 }
